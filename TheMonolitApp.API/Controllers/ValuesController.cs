967c1e307eecd7bf1060a23e0feb348200693f9a
nivrutti <nivrutti.khandade@gmail.com>
2020-09-25 09:54:56 -0400

    Initial Commit

---
+++ TheMonolitApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using TheMonolitApp.API.Data;
+
+namespace TheMonolitApp.API.Controllers
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        ILogger<ValuesController> _logger;
+        private readonly DataContext _context;
+
+        public ValuesController(ILogger<ValuesController> logger, DataContext context)
+        {
+            _context = context;
+            _logger = logger;
+            
+        }
+        // GET api/values
+        [HttpGet]
+        public async Task< IActionResult> Get()
+        {
+            var values =  await _context.Values.ToListAsync();
+            _logger.LogInformation("Test");
+            _logger.LogDebug("Debug Test");
+            return Ok(values);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task< ActionResult> Get(int id)
+        {
+            var value = await _context.Values.FirstOrDefaultAsync(v => v.Id == id);
+            return Ok(value);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ TheMonolitApp.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using TheMonolitApp.API.Models;
+
+namespace TheMonolitApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
