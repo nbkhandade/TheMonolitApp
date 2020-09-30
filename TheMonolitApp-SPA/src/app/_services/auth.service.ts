cfec7d1ac4464b12ed9e3b2b65453a724adfd607
nivrutti <nivrutti.khandade@gmail.com>
2020-09-30 19:01:16 -0400

    Resolver & Photo Gallary Added

--- TheMonolitApp-SPA/src/app/_services/auth.service.ts
+++ TheMonolitApp-SPA/src/app/_services/auth.service.ts
@@ -2,10 +2,11 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { environment } from 'src/environments/environment';
 @Injectable()
 export class AuthService {
 
-    baseUrl = 'http://localhost:5000/api/auth/';
+    baseUrl = environment.apiUrl +  'auth/';
     jwtHealper = new JwtHelperService();
     decodedToken: any;
     
